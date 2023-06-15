39c4722f0f05c4276ef24803792be1310ee75705
daflecardoso <daflesantos@gmail.com>
2023-06-14 22:07:52 -0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+import PackageDescription
+
+let package = Package(
+    name: "NetworkAnalyzer",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "NetworkAnalyzer",
+            targets: ["NetworkAnalyzer"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "NetworkAnalyzer",
+            dependencies: [],
+            path: "NetworkAnalyzer"),
+        .testTarget(
+            name: "NetworkAnalyzerTests",
+            dependencies: ["NetworkAnalyzer"],
+            path: "Tests")
+    ],
+    swiftLanguageVersions: [.v5]
+)
